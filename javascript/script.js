3c614491e365da0922b83715a9a82a6b4bb249d1
Cerescu Marius <marius.cerescu@gmail.com>
2022-03-04 08:33:49 +0200

    lab_3

--- javascript/script.js
+++ javascript/script.js
@@ -8,7 +8,6 @@ btnScrollToTop.addEventListener("click", function (){
  });
 });
 
-
 let viewportHeight = window.innerHeight;
 const btnScrollToSection = document.querySelector(".btn");
 btnScrollToSection.addEventListener("click", function (){
@@ -27,3 +26,50 @@ setInterval(function(){
         counter = 1;
     }
 }, 5000);
+
+function find_flight(){
+    var origin = document.getElementById("origin").value;
+    var destination = document.getElementById("destination").value;
+    var date = document.getElementById("date").value;
+
+var flight1 = document.getElementById("flight1");
+var flight2 = document.getElementById("flight2");
+var flight3 = document.getElementById("flight3");
+var flight4 = document.getElementById("flight4");
+var flight5 = document.getElementById("flight5");
+
+flight1.style.display = "none";
+flight2.style.display = "none";
+flight3.style.display = "none";
+flight4.style.display = "none";
+flight5.style.display = "none";
+   
+if(origin == "Chișinău" && destination == "Roma" && date == "15/03/2022"){
+    flight1.style.display = "block";
+}
+
+if(origin == "Roma" && destination == "Chișinău" && date == "27/06/2022"){
+    flight2.style.display = "block";
+}
+
+if(origin == "Chișinău" && destination == "Paris" && date == "18/09/2022"){
+    flight3.style.display = "block";
+}
+
+if(origin == "Londra" && destination == "Chișinău" && date == "04/08/2022"){
+    flight4.style.display = "block";
+}
+
+if(origin == "Chișinău" && destination == "Berlin" && date == "22/05/2022"){
+    flight5.style.display = "block";
+}
+}
+
+function find_all(){
+    flight1.style.display = "block";
+    flight2.style.display = "block";
+    flight3.style.display = "block";
+    flight4.style.display = "block";
+    flight5.style.display = "block";
+}
+
